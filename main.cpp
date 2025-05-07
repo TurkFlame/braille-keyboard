681e61781cf46d0c477229cedca1ab180fec2c6f
TurkFlame <130477009+TurkFlame@users.noreply.github.com>
2025-05-06 21:22:07 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,263 @@
+Código:
+
+int Button_A = 13;
+int Button_B = 12;
+int Button_C = 11;
+int Button_D = 10;
+int Button_E = 9;
+int Button_F = 8;
+int buzzer = 7;
+int Button_G = 6;
+int Button_H = 5;
+int Button_I = 4;
+int Button_J = 3;  // Corrigido de Button_j para Button_J
+int Button_K = 2;
+int Button_L = 52;
+int Button_M = 50;
+int Button_N = 48;
+int Button_O = 46;
+int Button_P = 44;
+int Button_Q = 42;
+int Button_R = 40;
+int Button_S = 38;
+int Button_T = 36;
+int Button_U = 34;
+int Button_V = 32;
+int Button_W = 30;
+int Button_X = 28;
+int Button_Y = 26;
+int Button_Z = 24;
+int Button_01 = 22;
+int Button_02 = 53;
+int Button_03 = 51;
+int Button_04 = 49;
+int Button_05 = 47;
+int Button_06 = 45;
+int Button_07 = 43;
+int Button_08 = 41;
+int Button_09 = 39;
+int Button_10 = 37;
+
+void setup() {
+  pinMode(Button_A, INPUT_PULLDOWN);
+  pinMode(Button_B, INPUT_PULLDOWN);
+  pinMode(Button_C, INPUT_PULLDOWN);
+  pinMode(Button_D, INPUT_PULLDOWN);
+  pinMode(Button_E, INPUT_PULLDOWN);
+  pinMode(Button_F, INPUT_PULLDOWN);
+  pinMode(Button_G, INPUT_PULLDOWN);
+  pinMode(Button_H, INPUT_PULLDOWN);
+  pinMode(Button_I, INPUT_PULLDOWN);
+  pinMode(Button_J, INPUT_PULLDOWN);
+  pinMode(Button_K, INPUT_PULLDOWN);
+  pinMode(Button_L, INPUT_PULLDOWN);
+  pinMode(Button_M, INPUT_PULLDOWN);
+  pinMode(Button_N, INPUT_PULLDOWN);
+  pinMode(Button_O, INPUT_PULLDOWN);
+  pinMode(Button_P, INPUT_PULLDOWN);
+  pinMode(Button_Q, INPUT_PULLDOWN);
+  pinMode(Button_R, INPUT_PULLDOWN);
+  pinMode(Button_S, INPUT_PULLDOWN);
+  pinMode(Button_T, INPUT_PULLDOWN);
+  pinMode(Button_U, INPUT_PULLDOWN);
+  pinMode(Button_V, INPUT_PULLDOWN);
+  pinMode(Button_W, INPUT_PULLDOWN);
+  pinMode(Button_X, INPUT_PULLDOWN);
+  pinMode(Button_Y, INPUT_PULLDOWN);
+  pinMode(Button_Z, INPUT_PULLDOWN);
+  pinMode(Button_01, INPUT_PULLDOWN);
+  pinMode(Button_02, INPUT_PULLDOWN);
+  pinMode(Button_03, INPUT_PULLDOWN);
+  pinMode(Button_04, INPUT_PULLDOWN);
+  pinMode(Button_05, INPUT_PULLDOWN);
+  pinMode(Button_06, INPUT_PULLDOWN);
+  pinMode(Button_07, INPUT_PULLDOWN);
+  pinMode(Button_08, INPUT_PULLDOWN);
+  pinMode(Button_09, INPUT_PULLDOWN);
+  pinMode(Button_10, INPUT_PULLDOWN);
+
+  pinMode(buzzer, OUTPUT);  // Adicionado ponto e vírgula
+}
+
+void loop() {
+  if(digitalRead(Button_A) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_B) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_C) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_D) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_E) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_F) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_G) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_H) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_I) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_J) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_K) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_L) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_M) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_N) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_O) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_P) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_Q) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_R) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_S) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_T) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_U) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_V) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_W) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_X) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_Y) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_Z) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_01) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_02) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_03) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_04) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_05) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_06) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_07) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_08) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_09) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+  if(digitalRead(Button_10) == LOW){
+    tone(buzzer, 2000);
+    delay(500);
+    noTone(buzzer);
+  }
+}
